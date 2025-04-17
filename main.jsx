7f3402383f4e40c56b82a0660e74909f9b3799c2
Mujtaba2625 <mujtaba2625@gmail.com>
2025-04-17 18:39:53 +0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PortfolioPage from './PortfolioPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PortfolioPage />
+  </React.StrictMode>
+)
